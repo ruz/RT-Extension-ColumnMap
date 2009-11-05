fe8dd8ec4cd2db3d0982b213a0d2e60c7352b706
Ruslan Zakirov <ruz@bestpractical.com>
2009-11-05 23:32:12 +0700

    mass update

--- lib/RT/Extension/ColumnMap.pm
+++ lib/RT/Extension/ColumnMap.pm
@@ -4,18 +4,24 @@ use warnings;
 
 package RT::Extension::ColumnMap;
 
+our $VERSION = '0.01';
+
 =head1 NAME
 
 RT::Extension::ColumnMap - bring ColumnMap to libraries
 
 =head1 DESCRIPTION
 
+This extension provides API for other extensions and/or
+local customizations.
 
 =cut
 
 our %MAP;
 
 $MAP{'RT::Record'} = {
+    _default => sub { return $_[0]->id },
+
     id => sub { return $_[0]->id },
 
     ( map { my $m = $_ .'Obj'; $_ => sub { return $_[0]->$m() } }
@@ -34,6 +40,16 @@ $MAP{'RT::Ticket'} = {
         TimeWorked
     ) ),
 };
+
+$MAP{'RT::Transaction'} = {
+    ( map { my $m = $_; $_ => sub { return $_[0]->$m() } }
+    qw(
+        Object
+        Type Field
+        OldValue NewValue
+    ) ),
+};
+
 foreach my $type (qw(Requestor Cc AdminCc)) {
     my $method = $type eq 'Requestor'? $type.'s': $type;
     my $map = {
@@ -45,6 +61,8 @@ foreach my $type (qw(Requestor Cc AdminCc)) {
 
 
 $MAP{'RT::User'} = {
+    _default => sub { return $_[0]->Name },
+
     ( map { my $m = $_; $_ => sub { return $_[0]->$m() } }
     qw(Name Comments Signature EmailAddress FreeformContactInfo
     Organization RealName NickName Lang EmailEncoding WebEncoding
@@ -53,11 +71,15 @@ $MAP{'RT::User'} = {
     Address1 Address2 City State Zip Country Timezone PGPKey) ),
 };
 
+use Storable qw(dclone);
+use Scalar::Util qw(blessed);
+
 
 use Regexp::Common qw(delimited);
-my $dequoter = $RE{delimited}{-delim=>q{'"}}{-esc=>'\\'}->action('dequote');
+use Regexp::Common::WithActions;
 
 my $re_quoted = qr/$RE{delimited}{-delim=>q{'"}}{-esc=>'\\'}/;
+my $dequoter = $RE{delimited}{-delim=>q{'"}}{-esc=>'\\'}->action('dequote');
 my $re_not_quoted = qr/[^{}'"\\]+/;
 
 my $re_arg_value = qr/ $re_quoted | $re_not_quoted /x;
@@ -68,7 +90,28 @@ my $re_field = qr/$re_field_name $re_arg*/x;
 
 my $re_column = qr/$re_field(?:\.$re_field)*/;
 
+my %LOCAL_RE = (
+    column => $re_column,
+    field_name => $re_field_name,
+    field => $re_field,
+    argument => $re_arg,
+);
+
+sub RE {
+    my $self = shift;
+    my $name = shift or die "Must specify regular expression to return";
+    return $LOCAL_RE{ lc $name } or die "Unknown regular expression '$name'";
+}
+
 sub Get {
+    my $self = shift;
+
+    my ($struct, $object) = $self->FindStart( @_ );
+
+    return $self->_Get( $struct, $object );
+}
+
+sub FindStart {
     my $self = shift;
     my %args = (String => undef, Objects => undef, @_);
 
@@ -83,51 +126,103 @@ sub Get {
         push @objects, \%tmp;
     }
     @objects = sort { @{$b->{struct}} <=> @{$b->{struct}} } @objects;
+    #Test::More::diag( Dumper( \@objects ) );
 
     my $prefix;
-    foreach $prefix ( @objects ) {
+    foreach ( @objects ) {
+        $prefix = $_;
         last if $self->IsPrefix( $prefix->{'struct'} => $struct );
     }
     return undef unless $prefix;
 
     splice @$struct, 0, scalar @{$prefix->{'struct'}};
 
-    return $self->_Get( $struct, $prefix->{'object'} );
+    return $struct, $prefix->{'object'};
 }
+
 
 sub _Get {
     my $self = shift;
     my $struct = shift;
     my $object = shift;
 
-    my ($entry, $callback) = $self->GetEntry( $struct, $object );
+    my ($entry, $callback) = $self->Entry( $struct, $object );
     die "boo" unless $entry;
 
     my %args = ( Arguments => [] );
-    foreach ( @{$struct}[0 .. @$entry-1] ) {
+    foreach ( grep ref $_, @{$struct}[0 .. @$entry-1] ) {
         push @{ $args{'Arguments'} }, $_->{'arguments'};
     }
-    if ( @$struct == @$entry ) {
-        $args{'Trailing'} = 1;
-    }
-
     my $value = $callback->( $object, %args );
-    if ( $args{'Trailing'} ) {
-        return $value;
-    } elsif ( blessed $value ) {
-        splice @$struct, 0, @$entry-1;
-        return $self->Get( $struct, $object );
+    splice @$struct, 0, scalar @$entry;
+    return $value;
+}
+
+sub Check {
+    my $self = shift;
+    my %args = @_;
+
+    my ($struct, $object) = $self->FindStart( @_ );
+    die "Couldn't find prefix" unless $struct;
+
+    return $self->_Check($struct, $object, $args{'Checker'} );
+}
+
+sub _Check {
+    my $self = shift;
+    my ($struct, $object, $checker) = @_;
+
+    my $value = $self->_Get( $struct, $object );
+    unless ( @$struct ) {
+        if ( blessed $value ) {
+            $value = $self->_Get(['_default'], $value);
+            return $checker->( $value );
+        } else {
+            return $checker->( $value );
+        }
     } else {
-        die "not trailing and not blessed";
+        die "Recieved value instead of object in the middle"
+            unless blessed $value;
+
+        if ( $value->isa('RT::SearchBuilder') ) {
+            while ( my $entry = $value->Next ) {
+                my $res = $self->_Check(
+                    Storable::dclone($struct), $entry, $checker
+                );
+                return $res if $res;
+            }
+        } elsif ( $value->isa('RT::Record') ) {
+            return $self->_Check( $struct, $value, $checker );
+        } else {
+            die "Don't know how to continue with $value";
+        }
     }
 }
 
-sub GetEntry {
+sub Entry {
+    my $self = shift;
+    my ($struct, $object) = (shift, shift);
+    
+    my ($entry, $callback) = $self->_Entry( $struct, $object, @_ );
+    if ( !$entry && blessed $object && $object->isa('RT::Record') ) {
+        ($entry, $callback) = $self->_Entry( $struct, 'RT::Record', @_ );
+    }
+    return ($entry, $callback) unless $entry;
+    return ($entry, $callback) unless ref $callback eq 'HASH';
+
+    $callback =
+        (@$struct == @$entry? $callback->{'Trailing'} : undef)
+        || $callback->{'Default'};
+
+    return ($entry, $callback);
+}
+
+sub _Entry {
     my $self = shift;
     my $struct = shift;
     my $object = shift;
 
-    my $type = ref $object;
+    my $type = ref $object || $object;
     my $map = $MAP{$type} or die "No map for $type";
 
     foreach my $e ( sort {length $b <=> length $a } keys %$map ) {
